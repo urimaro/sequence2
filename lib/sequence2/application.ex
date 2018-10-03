eec0f86f41693fe9ac7face9d104c07408bb4df4
Goh Matsumoto <urimaro@gmail.com>
2018-10-03 21:06:27 +0900

    P215 Modify to start the supervisor

--- lib/sequence2/application.ex
+++ lib/sequence2/application.ex
@@ -6,16 +6,6 @@ defmodule Sequence2.Application do
   use Application
 
   def start(_type, _args) do
-    # List all child processes to be supervised
-    children = [
-      # Starts a worker by calling: Sequence2.Worker.start_link(arg)
-      # {Sequence2.Worker, arg},
-      {Sequence2.Server, 123}
-    ]
-
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: Sequence2.Supervisor]
-    Supervisor.start_link(children, opts)
+    {:ok, _pid} = Sequence2.Supervisor.start_link(123)
   end
 end
