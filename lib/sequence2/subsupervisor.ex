c1434af9dc517a82f54249a7a3bbb885f0b3b179
Goh Matsumoto <urimaro@gmail.com>
2018-10-03 20:54:17 +0900

    P214 Add the sub supervisor which starts the sequence worker

---
+++ lib/sequence2/subsupervisor.ex
@@ -0,0 +1,13 @@
+defmodule Sequence2.SubSupervisor do
+  use Supervisor
+
+  def start_link(stash_pid) do
+    {:ok, _pid} = Supervisor.start_link(__MODULE__, stash_pid)
+  end
+
+  def init(stash_pid) do
+    child_processes = [worker(Sequence2.Server, [stash_pid])]
+    supervise child_processes, strategy: :one_for_one
+  end
+end
+
