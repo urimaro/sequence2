1796d9bc627a3b6849e6ec7e006a13f4c2c294b9
Goh Matsumoto <urimaro@gmail.com>
2018-10-09 00:18:15 +0900

    P221

--- mix.exs
+++ mix.exs
@@ -15,7 +15,8 @@ defmodule Sequence2.MixProject do
   def application do
     [
       extra_applications: [:logger],
-      mod: {Sequence2.Application, 456}
+      mod: {Sequence2.Application, 456},
+      registered: [Sequence2.Server]
     ]
   end
 
