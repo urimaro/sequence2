12045d46284f4ec7a99011232224af840e3f8cf1
Goh Matsumoto <urimaro@gmail.com>
2018-10-11 23:14:45 +0900

    P225 mix do deps.get, deps.compile

--- mix.exs
+++ mix.exs
@@ -26,6 +26,7 @@ defmodule Sequence2.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:exrm, "~> 1.0"}
     ]
   end
 end
