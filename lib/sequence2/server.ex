e3cd955e3863bc5b83d783a88d3812dd93579a9c
Goh Matsumoto <urimaro@gmail.com>
2018-10-14 23:42:30 +0900

    P229 サーバの状態のマイグレート

--- lib/sequence2/server.ex
+++ lib/sequence2/server.ex
@@ -1,7 +1,12 @@
 defmodule Sequence2.Server do
   use GenServer
 
-  @vsn "0"
+  require Logger
+
+  @vsn "1"
+  defmodule State do
+    defstruct current_number: 0, stash_pid: nil, delta: 1
+  end
 
   #####
   # 外部PI
@@ -24,15 +29,15 @@ defmodule Sequence2.Server do
 
   def init(stash_pid) do
     current_number = Sequence2.Stash.get_value stash_pid
-    {:ok, {current_number, stash_pid}}
+    {:ok, %State{current_number: current_number, stash_pid: stash_pid}}
   end
 
-  def handle_call(:next_number, _from, {current_number, stash_pid}) do
-    {:reply, current_number, {current_number + 1, stash_pid}}
+  def handle_call(:next_number, _from, state) do
+    {:reply, state.current_number, %{state | current_number: state.current_number + state.delta}}
   end
 
-  def handle_call({:set_number, new_number}, _from, {_current_number, stash_pid}) do
-    {:reply, new_number, {new_number, stash_pid}}
+  def handle_call({:set_number, new_number}, _from, state) do
+    {:reply, new_number, %{state | current_number: new_number}}
   end
 
   def handle_call({:factors, number}, _, _) do
@@ -41,16 +46,27 @@ defmodule Sequence2.Server do
 
   def factors(number), do: number + 1
 
-  def handle_cast({:increment_number, delta}, {current_number, stash_pid}) do
-    {:noreply, {current_number + delta, stash_pid}}
+  def handle_cast({:increment_number, delta}, state) do
+    {:noreply, %{state | current_number: state.current_number + delta, delta: delta}}
   end
 
   def format_status(_reason, [_pdict, state]) do
     [data: [{'State', "My current state is '#{inspect state}', and I'm happy"}]]
   end
 
-  def terminate(_reason, {current_number, stash_pid}) do
-    Sequence2.Stash.save_value stash_pid, current_number
+  def terminate(_reason, state) do
+    Sequence2.Stash.save_value state.stash_pid, state.current_number
+  end
+
+  def code_change("0", old_state = {current_number, stash_pid}, _extra) do
+    new_state = %State{current_number: current_number,
+                       stash_pid:      stash_pid,
+                       delta:          1
+    }
+    Logger.info "Changing code from 0 to 1"
+    Logger.info inspect(old_state)
+    Logger.info inspect(new_state)
+    {:ok, new_state}
   end
 end
 
